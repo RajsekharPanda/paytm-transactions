4137653b5784e6415facb8eb1274d3a7b7ff3c19
RajsekharPanda <rajsekharpandawork@gmail.com>
2024-04-22 03:48:30 +0530

    backend done

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
+const cors = require('cors')
+const rootRouter = require('backend/routes/index.js')
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+const mainRouter = require('./routes/index');
 
 
+app.use('/api/v1/', rootRouter)
+app.listen(3000)
